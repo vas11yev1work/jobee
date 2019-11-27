72f8a29dd0c13e3bef5fa13ef8bde01118aab097
vas11yev1work <49168917+vas11yev1work@users.noreply.github.com>
2019-11-28 00:54:00 +0300

    Update

--- frontend/src/components/Interview.vue
+++ frontend/src/components/Interview.vue
@@ -43,7 +43,7 @@
                 name: '',
                 phone: '',
                 loading: false,
-                sent: true
+                sent: false
             }
         },
         methods: {
