93af8b0cf2e87b095144fa2ca1f07e5e8453f78c
Ajbolitt76 <78945qw@mail.ru>
2019-12-04 18:21:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('trust proxy', true)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
